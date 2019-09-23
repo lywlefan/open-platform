157d82b8173f6720502396940238a328ce3f1475
lywlefan <330334064@qq.com>
2019-09-23 23:41:19 +0800

    添加eureka

---
+++ open-platform-base/open-platform-eureka/src/main/java/com/shareyuan/Application.java
@@ -0,0 +1,14 @@
+package com.shareyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
